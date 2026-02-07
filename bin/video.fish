c90bc2cc2c979ff9609230f1d87f128b155e6e9f
Holly Becker <becker.holly@gmail.com>
2026-02-06 22:41:40 -0800

    More video helper scripts

--- bin/video.fish
+++ bin/video.fish
@@ -1,3 +1,4 @@
+# Max 50MiB
 # Resize video to max 1000 px tall
 ffmpeg -i input.mp4 -vf "scale=h=1024:w=-1" output.mp4
 # Resize video to max 1000 px wide
@@ -5,6 +6,9 @@ ffmpeg -i input.mp4 -vf "scale=w=1024:h=-1" output.mp4
 
 # Shorten video
 ffmpeg -t 00:00:20 -i input.mp4 -async 1 -c copy output-short.mp4
+
+# Drop first x seconds
+ffmpeg -i input.mp4 -ss 00:00:08 -async 1 -c copy output-short.mp4
 
 # Make a thumbnail from the first frame of video
 ffmpeg -i input.mp4 -frames:v 1 -an -vf "setsar=1" -y output.jpg
