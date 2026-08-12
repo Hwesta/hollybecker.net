cd9297d09719da2c9fa2a606e6dfa35c304e4a06
Holly Becker <becker.holly@gmail.com>
2026-08-11 17:07:58 -0700

    Update video helper scripts
    Match the actual commands I tend to use, including fish shell variables

--- bin/video.fish
+++ bin/video.fish
@@ -1,17 +1,18 @@
 # Max 50MiB
+set name deer
 # Resize video to max 1000 px tall
-ffmpeg -i input.mp4 -vf "scale=h=1024:w=-1" output.mp4
+ffmpeg -i ~/downloads/$name.mp4 -vf "scale=h=1024:w=-1" $name.mp4
 # Resize video to max 1000 px wide
-ffmpeg -i input.mp4 -vf "scale=w=1024:h=-1" output.mp4
+ffmpeg -i ~/downloads/$name.mp4 -vf "scale=w=1024:h=-1" $name.mp4
 
 # Shorten video
-ffmpeg -t 00:00:20 -i input.mp4 -async 1 -c copy output-short.mp4
+ffmpeg -t 00:00:20 -i ~/downloads/$name.mp4 -async 1 -c copy $name-short.mp4
 
 # Drop first x seconds
-ffmpeg -i input.mp4 -ss 00:00:08 -async 1 -c copy output-short.mp4
+ffmpeg -i ~/downloads/$name.mp4 -ss 00:00:08 -async 1 -c copy $name-short.mp4
 
 # Make a thumbnail from the first frame of video
-ffmpeg -i input.mp4 -frames:v 1 -an -vf "setsar=1" -y output.jpg
+ffmpeg -i ~/downloads/$name.mp4 -frames:v 1 -an -vf "setsar=1" -y $name.jpg
 
 # remove audio
 -an
